cf50f743a30e489091f3195996cc2a6cc35d08b2
ineideot <ineideot@mail.ru>
2022-10-18 16:31:22 +0300

    Example005_ConditionIfElse

---
+++ Lecture_1/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "денис")
+{
+    Console.WriteLine("Ура, это же Денис!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
