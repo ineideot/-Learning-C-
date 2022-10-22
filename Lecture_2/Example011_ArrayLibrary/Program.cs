2dcd315d942ac96881a9b23f2ceb41e78361e402
Денис Игоревич <115458253+ineideot@users.noreply.github.com>
2022-10-22 18:33:10 +0300

    Example011_ArrayLibrary

---
+++ Lecture_2/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿int[] array = new int[12]; //new new int[12] - создай новый массив в котором будет 12 элементов
+
+
+void FillArray(int[] collection)  //Создадим пустой масив с именем collection.
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 19);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Indexof(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+FillArray(array);
+array[4]= 4;
+array[5]= 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 4);
+Console.WriteLine(pos);
