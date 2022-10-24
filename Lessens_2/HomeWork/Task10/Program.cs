017ce2681273286d0d4d2b1fafe8a43e97f560ee
Денис Игоревич <115458253+ineideot@users.noreply.github.com>
2022-10-24 15:46:44 +0300

    Task10

---
+++ Lessens_2/HomeWork/Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Input triple digits ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SearchTwoDigit(int arg1)
+{
+int result = ((arg1- (arg1 /100 * 100))- arg1 %10)/ 10;
+Console.WriteLine($"Second digit {result}");
+}
+
+SearchTwoDigit(num);
