9b79c94175aa24667a1099c655af1485715bdc33
Денис Игоревич <115458253+ineideot@users.noreply.github.com>
2022-10-24 15:48:31 +0300

    Task13

---
+++ Lessens_2/HomeWork/Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+
+Console.Write("Input natural number: ");
+uint nNumber = Convert.ToUInt16(Console.ReadLine());
+
+string nNumberText = Convert.ToString(nNumber);
+
+if (nNumberText.Length > 2)
+{
+    Console.WriteLine("Third digit: " + nNumberText[2]);
+}
+else
+{
+    Console.WriteLine("No third digit");
+}
